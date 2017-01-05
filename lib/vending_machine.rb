527a07e62bc3a6f19c9111cad544d23d8cc75ce3
Amara Lovato <amaracuenca@gmail.com>
2017-01-05 09:32:18 -0700

    Adds method add_snack

---
+++ lib/vending_machine.rb
@@ -0,0 +1,14 @@
+require './lib/snack'
+
+class VendingMachine
+  attr_reader :inventory
+
+  def initialize
+    @inventory = []
+  end
+
+  def add_snack(snack)
+    @inventory << snack
+  end
+
+end
